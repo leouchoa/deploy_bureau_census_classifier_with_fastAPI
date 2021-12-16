dd0dc4adee4e016d80601ab3b89ebf4ca83fc07a
Leonardo <leonardouchoa.pedreira@gmail.com>
2021-12-15 21:08:44 -0300

    feat: reverse 0/1 result of label binarizer into original classes.
    Convenience function to return the original <=50k or >50k classes
    instead of 0/1

--- modeling_pipeline/ml/model.py
+++ modeling_pipeline/ml/model.py
@@ -5,6 +5,7 @@ Utility functions for:
 """
 import os
 import pickle as pkl
+from typing import List
 
 from sklearn.ensemble import RandomForestClassifier as rf
 
@@ -65,3 +66,38 @@ def save_model(path_to_save: str, clf) -> None:
     """
     path_to_save = os.path.join(path_to_save, "classifier.pkl")
     pkl.dump(clf, open(path_to_save, "wb"))
+
+
+def get_original_y_class(pred: int, label_binarizer) -> List:
+    """
+    Revert from 0/1 to original y class <=50k/>50k.
+    Meant to be specially used within the API for convenience reasons
+    """
+    return label_binarizer.inverse_transform(pred).tolist()
+
+
+if __name__ == "__main__":
+    import pandas as pd
+
+    from modeling_pipeline.ml.data import process_data
+    from modeling_pipeline.ml.evaluate_model import load_model_and_processors
+    from modeling_pipeline.ml.utils import get_cat_features
+
+    data_path = "data/train_test_split/test_df.csv"
+
+    test_data = pd.read_csv(data_path)
+
+    clf, encoder, lb = load_model_and_processors("model")
+
+    X_eval, *_ = process_data(
+        X=test_data,
+        categorical_features=get_cat_features(),
+        label=None,
+        training=False,
+        encoder=encoder,
+        lb=lb,
+    )
+
+    preds = inference(clf, X_eval)
+
+    print(get_original_y_class(pred=preds, label_binarizer=lb))
